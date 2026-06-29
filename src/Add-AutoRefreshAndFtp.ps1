0d80cc95c49462480c14f8aed03237b81a420265
Matt Desai <mattdesai@hotmail.com>
2026-06-29 16:27:01 -0400

    use custom domains; single ftp upload script; add standings pages; major changes to ftp.ps1

--- src/Add-AutoRefreshAndFtp.ps1
+++ src/Add-AutoRefreshAndFtp.ps1
@@ -3,93 +3,185 @@ Param(
     [Int] $LoopTime = 0
 )
 
-$wsh = New-Object -ComObject WScript.Shell
-$RunningFile = $PSScriptRoot + "\publishrunning.txt"
+$RunningFile  = Join-Path $PSScriptRoot "publishrunning.txt"
+$stopFtpFile  = Join-Path $PSScriptRoot "stopftp.txt"
+$psStatusFile = ".\html\jm.golfpool.us\htdocs\psStatus.html"
+
 
 function Keep-Awake()
 {
-	$wsh.SendKeys('+{capslock}')
-	$wsh.SendKeys('+{capslock}')
+	# Changed this to ensure sleep is disabled when plugged in
+	# $wsh = New-Object -ComObject WScript.Shell
+	# $wsh.SendKeys('+{capslock}')
+	# $wsh.SendKeys('+{capslock}')
+	Write-Host "Disabled standy/sleep via powercfg"
+	powercfg /change standby-timeout-ac 0
 }
 
-function CheckForExit()
+function Check-ForExit()
 {
-	$stopFtpFile = $PSScriptRoot + "\stopftp.txt"
-	
-
 	if (Test-Path $stopFtpFile -PathType Leaf)
 	{
 		Write-Host "`rEnding."
 		Remove-item $stopFtpFile -ErrorAction Ignore
-		Remove-Item $RunningFile -ErrorAction Ignore
-		exit 5
+		return $true
 	}
+	return $false
 }
 
-Set-Location -Path "C:\pga"
-Write-Host "Add-AutorefreshAndFtp Running.  LoopTime = $($LoopTime);  dir=$($(Get-Location).Path)"
-"Running" | Out-File $RunningFile
+function Insert-Html {
+	param(
+		[string]$origFile
+	)
 
+	# $x = (gc $origFile)  -as [Collections.ArrayList]
+	# $x.Insert(7,'<meta http-equiv="refresh" content="60" />')
+	# $x.Insert(7,'<meta http-equiv="Pragma" content="no-cache" />')
+	# $x.Insert(7,'<meta http-equiv="Expires" content="0" />')
+	# $x.Insert(7,'<meta http-equiv="Cache-control" content="no-cache, must-revalidate" />')
+	# $x.Insert(7,"<style> {margin:0;padding:0;} html { background: url('bg.jpg') no-repeat center center fixed; background-color: rgba(255,255,255,0.81); background-blend-mode: lighten; background-size: cover;} </style>")
+	# # Used to have a sleep 1 before deleting all the temp files.  sharing violation?
+	# Remove-Item $origFile
+	# return ,$x
+
+	# Read all lines natively as a string array
+	$lines = Get-Content $origFile
+
+	$tagsToInsert = @(
+		'<meta http-equiv="refresh" content="60" />',
+		'<meta http-equiv="Pragma" content="no-cache" />',
+		'<meta http-equiv="Expires" content="0" />',
+		'<meta http-equiv="Cache-control" content="no-cache, must-revalidate" />',
+		"<style> {margin:0;padding:0;} html { background: url('bg.jpg') no-repeat center center fixed; background-color: rgba(255,255,255,0.81); background-blend-mode: lighten; background-size: cover;} </style>"
+	)
+
+	$output = @()
+	$output += $lines[0..6]
+	$output += $tagsToInsert
+	if ($lines.Count -gt 7) {
+		$output += $lines[7..($lines.Count - 1)]
+	}
+
+	Remove-Item $origFile -Force -ErrorAction Ignore
+
+	return ,$output
+}
+
+
+# Initialization
+Set-Location -Path "C:\pga" -ErrorAction Stop
+Remove-item $stopFtpFile -ErrorAction Ignore
+Keep-Awake
+$currentDate = Get-Date -Format MM/dd
+
+$statusMsg = "$($currentDate): Add-AutorefreshAndFtp Running.  LoopTime = $($LoopTime);  dir=$($(Get-Location).Path)"
+Write-Host $statusMsg
+"$statusMsg</br>" | Out-File -FilePath $psStatusFile -Append
+$statusMsg | Out-File $RunningFile
+
+
+Write-Host "1=No Update in >30 mins; 2=Updates Ready; 4=Status ready"
+$lastUploadTime = Get-Date
+
+try {
 do {
 
-	Keep-Awake
-	CheckForExit
+	try {
+		if (Check-ForExit) { break }
 
-	Write-Host -NoNewline "."
+		$currentTime = Get-Date
+	        $state = 0
 
-	if (Test-Path '.\html\JoeMagnetico.htm')
-	{
-		Write-Verbose "new html files to upload"
-
-		(Get-Content .\html\JoeMagnetico.htm).Replace(";color:white;",";color:transparent;") | Set-Content .\html\tempJ.html
-		(Get-Content .\html\Micro.htm).Replace(";color:white;",";color:transparent;") | Set-Content .\html\tempM.html
-
-		$x = (gc .\html\tempJ.html) -as [Collections.ArrayList]
-		$y = (gc .\html\tempM.html) -as [Collections.ArrayList]
-
-		if (Test-Path '.\html\LeadChanges.htm')
+		if ($currentDate -lt (Get-Date -Format MM/dd))
 		{
-			Copy-Item -Path ".\html\LeadChanges.htm"      -Destination ".\html\LeadChanges.html"        # just for consistency 
-			Copy-Item -Path ".\html\LeadChangesMicro.htm" -Destination ".\html\LeadChangesMicro.html"   # just for consistency 
+			$currentDate = Get-Date -Format MM/dd
+			write-Host "`nNew date: $currentDate"
+			"$currentDate</br>" | Out-File -FilePath $psStatusFile -Append
 		}
 
-        Start-Sleep 1
-		Remove-Item '.\html\JoeMagnetico.htm'
-		Remove-Item '.\html\Micro.htm'
-		Remove-Item '.\html\tempJ.html'
-		Remove-Item '.\html\tempM.html'
-		
-		$x.Insert(7,'<meta http-equiv="refresh" content="60" />')
-		$x.Insert(7,'<meta http-equiv="Pragma" content="no-cache" />')
-		$x.Insert(7,'<meta http-equiv="Expires" content="0" />')
-		$x.Insert(7,'<meta http-equiv="Cache-control" content="no-cache, must-revalidate" />')
-		$x.Insert(7,"<style> {margin:0;padding:0;} html { background: url('bg.jpg') no-repeat center center fixed; background-color: rgba(255,255,255,0.81); background-blend-mode: lighten; background-size: cover;} </style>")
+		Write-Host -NoNewline "."
 
-		$x | Set-Content '.\html\index.html' -Force # -Encoding UTF8
-
-		$y.Insert(7,'<meta http-equiv="refresh" content="60" />')
-		$y.Insert(7,'<meta http-equiv="Pragma" content="no-cache" />')
-		$y.Insert(7,'<meta http-equiv="Expires" content="0" />')
-		$y.Insert(7,'<meta http-equiv="Cache-control" content="no-cache, must-revalidate" />')
-		$y.Insert(7,"<style> {margin:0;padding:0;} html { background: url('bg.jpg') no-repeat center center fixed; background-color: rgba(255,255,255,0.81); background-blend-mode: lighten; background-size: cover;} </style>")
-
-		$y | Set-Content '.\html\micro.html' -Force # -Encoding UTF8
-
-		Write-Verbose "call winscp to upload..."
-		if (Test-Path '.\html\LeadChanges.htm')
+		$deltaTime = $currentTime - $lastUploadTime
+		if ($deltaTime.TotalMinutes -ge 30)
 		{
-			Start-Process -wait "\Program Files (x86)\WinSCP\WinSCP.exe" "/script=ftpSettingsLeaders.txt"
-			Remove-Item '.\html\LeadChanges.htm'
-			Remove-Item '.\html\LeadChangesMicro.htm'
-		} else {
-			Start-Process -wait "\Program Files (x86)\WinSCP\WinSCP.exe" "/script=ftpSettingsBoth.txt"
+			$state = $state -bor 1
 		}
 
-		Write-Host "`r$((Get-Date -Format "HH-mm-ss.MM-dd").ToString()) - ftp upload complete."
+		if (Test-Path '.\html\JoeMagnetico.htm')
+		{
+			# Immediately copy and remove
+			(Get-Content .\html\JoeMagnetico.htm -Raw).Replace(";color:white;",";color:transparent;") | Set-Content .\html\tempJ.html
+			(Get-Content .\html\Micro.htm -Raw).Replace(";color:white;",";color:transparent;") | Set-Content .\html\tempM.html
+			Remove-Item '.\html\JoeMagnetico.htm', '.\html\Micro.htm' -Force -ErrorAction Ignore
+
+			$hasStandings = $false
+			if (Test-Path '.\html\JMStandings.htm') {
+				$hasStandings = $true
+				(Get-Content .\html\JMStandings.htm -Raw).Replace(";color:white;",";color:transparent;") | Set-Content .\html\tempJS.html
+				(Get-Content .\html\MicroStandings.htm -Raw).Replace(";color:white;",";color:transparent;") | Set-Content .\html\tempMS.html
+				Remove-Item '.\html\JMStandings.htm', '.\html\MicroStandings.htm' -Force -ErrorAction Ignore
+			}
+
+			# Write-Verbose "new html files to upload"
+			$state = $state -bor 2
+
+			$x = Insert-Html -origFile '.\html\tempJ.html'
+			$y = Insert-Html -origFile '.\html\tempM.html'
+
+			$x | Set-Content '.\html\jm.golfpool.us\htdocs\index.html' -Force # -Encoding UTF8
+			copy .\html\jm.golfpool.us\htdocs\index.html .\html\htdocs\index.html
+
+			$y | Set-Content '.\html\micro.golfpool.us\htdocs\index.html' -Force # -Encoding UTF8
+			copy .\html\micro.golfpool.us\htdocs\index.html .\html\htdocs\micro.html
+
+			if ($hasStandings) {
+				$a = Insert-Html -origFile '.\html\tempJS.html'
+				$a | Set-Content '.\html\jm.golfpool.us\htdocs\Standings.html' -Force # -Encoding UTF8
+
+				$b = Insert-Html -origFile '.\html\tempMS.html'
+				$b | Set-Content '.\html\micro.golfpool.us\htdocs\Standings.html' -Force # -Encoding UTF8
+			}
+		}
+
+		if (Test-Path '.\html\status.htm')
+		{
+			# Immediately copy and remove
+			Copy-Item -Path .\html\status.htm -Destination .\html\jm.golfpool.us\htdocs\status.html -ErrorAction Ignore 
+			Copy-Item -Path .\html\status.htm -Destination .\html\micro.golfpool.us\htdocs\status.html -ErrorAction Ignore 
+			Remove-Item '.\html\status.htm' -ErrorAction Ignore 
+			$state = $state -bor 4
+		}
+
+		if ($state -gt 0)
+		{
+			Write-Verbose "call winscp to upload..."
+			$v = "$state - $((Get-Date -Format "HH:mm:ss").ToString()) - ftp."
+			Write-Host $v
+			"$v</br>" | Out-File -FilePath $psStatusFile -Append
+			Copy-Item -Path  $psStatusFile -Destination .\html\micro.golfpool.us\htdocs -ErrorAction Ignore
+			$lastUploadTime = Get-Date
+
+			Start-Process -wait "\Program Files (x86)\WinSCP\WinSCP.exe" "/script=ftpUpload.txt"
+		}
+	}
+	catch [System.Management.Automation.PipelineStoppedException] {
+		# If Ctrl+C happens to hit inside the try block, throw to outer finally block
+		throw 
+        }
+	catch {
+		Write-Error $($_.Exception.Message)
 	}
 
 	Start-Sleep $LoopTime
 
 } while ($LoopTime)
 
-Remove-Item $RunningFile -ErrorAction Ignore
+}
+finally
+{
+	write-host "`nExiting."
+	Remove-Item $RunningFile -ErrorAction Ignore
+
+	# if it exits prematurely/crash, dont want machine to sleep
+	# powercfg /change standby-timeout-ac 90
+}
