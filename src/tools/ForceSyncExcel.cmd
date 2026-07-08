bef29e6077294b04d2f13dceef16b0cd54a07430
Matt Desai <mattdesai@hotmail.com>
2026-07-08 17:30:37 -0400

    minor: decom old site and redirect to new

--- src/tools/ForceSyncExcel.cmd
+++ src/tools/ForceSyncExcel.cmd
@@ -1,6 +1,9 @@
 @echo off
 echo Force syncing excel sheets when forms are submitted...
 echo.
+echo JM https://sl5wg-my.sharepoint.com/:x:/r/personal/golfpooladmin_sl5wg_onmicrosoft_com/Documents/2026%20British%20Open%20Golf%20Pool%20Entry%20Form%20-%20JM.xlsx?d=w6cad40a724d145c19cf20d97e5d6ed7d
+echo MI https://sl5wg-my.sharepoint.com/:x:/r/personal/golfpooladmin_sl5wg_onmicrosoft_com/Documents/2026%20British%20Open%20Golf%20Pool%20Entry%20Form%20-%20Micro.xlsx?d=w2416140c2ee2402a92b3fb3fb81b2ed8
+echo.
 
 :top
 
